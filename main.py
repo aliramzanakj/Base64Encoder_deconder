85949edf1342ac8b028ae0e1419c72f4de6f820f
aliramzan <aliramzanakj@gmai.com>
2024-01-09 09:53:43 +0500

    Encrypt Function

---
+++ main.py
@@ -0,0 +1,10 @@
+import base64
+
+def encrypt_pass(password):
+    encoded_pass = base64.b16encode(password.encode())
+    return encoded_pass
+
+
+password = input("ENTER YOUR PASSWORD:")
+
+print("Your Encoded Password : {}".format(encrypt_pass(password)))
